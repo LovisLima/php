264ae0412ac30b8fa9723ea4b160a0b36a32b157
unknown <wagner.lima.andrade@alumni.usp.br>
2022-01-26 09:27:31 -0300

    second commit

--- formulario/conexao.php
+++ formulario/conexao.php
@@ -1,1 +1,11 @@
 <?php
+//script de conexao
+
+$servername ="localhost";// padrao -server local
+$database ="formulario_estoque"; //alterar conforme o nome do seu banco de dados
+$username = "root";// padrao - root
+$password = "";//senha de conexão do banco de dados.
+//create connection
+$conexao = mysqli_connect($servername, $username, $password, $database);
+
+$inserir = mysqli_query($conexao, $sql);
--- formulario/listarProdutos.php
+++ formulario/listarProdutos.php
@@ -8,10 +8,57 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
           integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn"
           crossorigin="anonymous">
+
+    <script src="https://kit.fontawesome.com/4c5260c39c.js" crossorigin="anonymous"></script>
 </head>
 <body>
-<div class="container">
-    
+<div class="container" style ="margin-top:  40px ">
+    <br>
+<h3>Lista de Produtos</h3>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Nro Produto</th>
+            <th scope="col">Nome Produto</th>
+            <th scope="col">Categoria</th>
+            <th scope="col">Quantidade</th>
+            <th scope="col">Fornecedor</th>
+            <th scope="col">Ação</th>
+        </tr>
+        </thead>
+
+
+                <?php
+                    include 'conexao.php';
+                    $sql = "SELECT * FROM estoque";
+                $servername ="localhost";// padrao -server local
+                $database ="formulario_estoque"; //alterar conforme o nome do seu banco de dados
+                $username = "root";// padrao - root
+                $password = "";//senha de conexão do banco de dados.
+                $conexao = mysqli_connect($servername, $username, $password, $database);
+                $busca = mysqli_query($conexao,$sql);
+
+                while ($array = mysqli_fetch_array($busca)){
+                    $id_estoque = $array['id_estoque'];
+                    $nroproduto = $array['nroproduto'];
+                    $nomeproduto = $array['nomeproduto'];
+                    $categoria = $array['categoria'];
+                    $quantidade = $array['quantidade'];
+                    $fornecedor = $array['fornecedor'];
+                ?>
+            <tr>
+                <td><?php echo $nroproduto ?> </td>
+                <td><?php echo $nomeproduto ?> </td>
+                <td><?php echo $categoria ?> </td>
+                <td><?php echo $quantidade ?> </td>
+                <td><?php echo $fornecedor ?> </td>
+                <td><a class="btn btn-warning btn-sm" href="editarProduto.php?id=<?php echo
+                $id_estoque ?>"role="button"><i class="far fa-edit"></i>&nbsp;&nbsp;Editar</a></td>
+            </tr>
+                <?php } ?>
+
+    </table>
+
 </div>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
